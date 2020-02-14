63395ccde6d525bd5f66b3fc39d02033af93b111
Robin Donnay <robin.donnay@etu.univ-lyon1.fr>
2020-02-14 19:14:10 +0100

    It's been a while

---
+++ Assets/Scripts/RealCharacterKiVe.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealCharacterKiVe : MonoBehaviour
+{
+
+    private Transform headset = null;
+    private Transform leftHand = null;
+    private Transform rightHand = null;
+
+    
+    const int HEAD = 0, NECK = 1, RIGHT_SHOULDER = 2, RIGHT_ELBOW = 3, 
+        RIGHT_HAND=4, LEFT_SHOULDER = 5, LEFT_ELBOW = 6, LEFT_HAND = 7,
+        RIGHT_HIPS = 8, RIGHT_KNEE = 9, RIGHT_FOOT = 10, LEFT_HIPS = 11,
+        LEFT_KNEE = 12, LEFT_FOOT = 13;
+
+    private List<Transform> joints = new List<Transform>();
+
+    private void getVRComponent()
+    {
+        // Search and hopefully find the CameraRig component of SteamVR plugin (the component should be at root of scene)
+        Transform cameraRig = GameObject.Find("/[CameraRig]").transform;
+        if(cameraRig == null){
+            Debug.LogError("Can't find CameraRig (of the steamVR plugin)");
+        } else {
+            Debug.Log("CameraRig of steamVR found");
+            headset = cameraRig.Find("Camera");
+            leftHand = cameraRig.Find("Controller (left)");
+            rightHand = cameraRig.Find("Controller (right)");
+            if(leftHand == null){Debug.LogError("No left controller found");}
+            if(rightHand == null){Debug.LogError("No right controller found");}
+        }
+    }
+
+    public Transform getHeadset(){
+        return headset;
+    }
+
+    public Transform getLeftHand(){
+        return leftHand;
+    }
+
+    public Transform getRightHand(){
+        return rightHand;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        getVRComponent();
+
+        // Get all transform we need for kiVe
+        for(int i=0;i<14;++i){
+            switch(i){
+                case HEAD:              {joints.Add(GameObject.Find("mixamorig:Head").transform);           break;}
+                case NECK:              {joints.Add(GameObject.Find("mixamorig:Neck").transform);           break;}
+                case RIGHT_SHOULDER:    {joints.Add(GameObject.Find("mixamorig:RightShoulder").transform);  break;}
+                case RIGHT_ELBOW:       {joints.Add(GameObject.Find("mixamorig:RightForeArm").transform);   break;}
+                case RIGHT_HAND:        {joints.Add(GameObject.Find("mixamorig:RightHand").transform);      break;}
+                case LEFT_SHOULDER:     {joints.Add(GameObject.Find("mixamorig:LeftForeArm").transform);    break;}
+                case LEFT_ELBOW:        {joints.Add(GameObject.Find("mixamorig:RightShoulder").transform);  break;}
+                case LEFT_HAND:         {joints.Add(GameObject.Find("mixamorig:LeftHand").transform);       break;}
+                case RIGHT_HIPS:        {joints.Add(GameObject.Find("mixamorig:RightUpLeg").transform);     break;}
+                case RIGHT_KNEE:        {joints.Add(GameObject.Find("mixamorig:RightLeg").transform);       break;}
+                case RIGHT_FOOT:        {joints.Add(GameObject.Find("mixamorig:RightFoot").transform);      break;}
+                case LEFT_HIPS:         {joints.Add(GameObject.Find("mixamorig:LeftUpLeg").transform);      break;}
+                case LEFT_KNEE:         {joints.Add(GameObject.Find("mixamorig:LeftLeg").transform);        break;}
+                case LEFT_FOOT:         {joints.Add(GameObject.Find("mixamorig:LeftFoot").transform);       break;}
+                default : break;
+            }
+        }
+    }
+
+    private void asICanSkeletonUpdate(){
+        joints[HEAD].transform.position = headset.position;  
+        joints[LEFT_HAND].transform.position = leftHand.position;  
+        joints[RIGHT_HAND].transform.position = rightHand.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(headset == null || leftHand == null || rightHand == null) {
+            getVRComponent();
+        }
+        asICanSkeletonUpdate();
+    }
+}
---
+++ Assets/Scripts/ViveInput.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour
+{
+    public SteamVR_Action_Single squeezeAction;
+
+    public SteamVR_Action_Vector2 touchPadAction;
+
+    private GameObject currentObj;
+    private bool isChoosingItem;
+
+    private int currentObjId = -1;
+
+    private bool hasChangeItem(Vector2 touchPadValue){
+        if(touchPadValue.x<0 && touchPadValue.y<0 && currentObjId == 1){
+            return false;
+        } else if(touchPadValue.x<0 && touchPadValue.y>0 && currentObjId == 2) {
+            return false;
+        } else if(touchPadValue.x>0 && touchPadValue.y<0 && currentObjId == 3){
+            return false;
+        } else if(touchPadValue.x>0 && touchPadValue.y>0 && currentObjId == 4){
+            return false;
+        } else  {
+            return true;
+        }
+    }
+
+    private void chooseItem(Vector2 touchPadValue) {
+        if(touchPadValue.x<0 && touchPadValue.y<0){
+            currentObj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            currentObjId = 1;
+        } else if(touchPadValue.x<0 && touchPadValue.y>0) {
+            currentObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            currentObjId = 2;
+        } else if(touchPadValue.x>0 && touchPadValue.y<0){
+            currentObj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            currentObjId = 3;
+        } else if(touchPadValue.x>0 && touchPadValue.y>0){
+            currentObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            currentObjId = 4;
+        }
+        currentObj.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        currentObj.transform.position = new Vector3(0, 2, 0);
+        // TODO Maybe add script for item spawned during the game (concept of the game)
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 touchPadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+        if(touchPadValue != Vector2.zero){
+            //print(touchPadValue);
+            if(!isChoosingItem) {
+                isChoosingItem = true;
+                chooseItem(touchPadValue);
+            } else if(hasChangeItem(touchPadValue)) {
+                Destroy(currentObj);
+                chooseItem(touchPadValue);
+            }
+        } else {
+            isChoosingItem = false;
+            if(currentObj != null){
+                Destroy(currentObj);
+            }
+        }
+
+        float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
+        if(triggerValue > 0f){
+            Rigidbody t = currentObj.AddComponent<Rigidbody>();
+            currentObj = null;
+            //print(triggerValue);
+        }
+    }
+}
